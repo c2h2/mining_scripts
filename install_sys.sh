0cd0a2a50f91e1b395613f3f1d4379ca5a58ee0f
Yiling Cao <yiling.cao@gmail.com>
2017-09-06 14:03:07 +0800

    Update install_sys.sh

--- install_sys.sh
+++ install_sys.sh
@@ -1,7 +1,10 @@
 sudo apt-get update
-sudo apt-get install -y git p7zip-full
+sudo apt-get install -y git p7zip-full byobu
 sudo apt-get install -y libjansson4 libcurl3 libcurl4-openssl-dev
 
 
 wget http://lisk.love/files/mining20170906.tar.xz
 tar -xvf mining20170906.tar.xz
+
+
+./install_nv.sh
