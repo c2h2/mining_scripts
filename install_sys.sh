c9dd5c9c7d514f5f670934f1168791bbb3a1398a
Yiling Cao <yiling.cao@gmail.com>
2017-09-06 14:10:57 +0800

    fix clone and path problem.

--- install_sys.sh
+++ install_sys.sh
@@ -2,9 +2,12 @@ sudo apt-get update
 sudo apt-get install -y git p7zip-full byobu
 sudo apt-get install -y libjansson4 libcurl3 libcurl4-openssl-dev
 
+git clone https://github.com/c2h2/mining_scripts.git
+
+
 
 wget http://lisk.love/files/mining20170906.tar.xz
 tar -xvf mining20170906.tar.xz
 
 
-./install_nv.sh
+mining_scripts/install_nv.sh
